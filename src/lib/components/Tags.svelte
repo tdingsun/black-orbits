80a2872c604325c3d125067ea196f30c090b6681
tdingsun <tiger.dingsun@gmail.com>
2025-03-22 11:14:59 -0400

    tags styling

--- src/lib/components/Tags.svelte
+++ src/lib/components/Tags.svelte
@@ -17,7 +17,7 @@
     <div class="flex flex-col gap-2">
         {#each tags.allTags as tag}
         <div onclick={() => toggleTag(tag)}
-            class="text-xs cursor-pointer hover:border-primary-text border w-max px-1 py-0.5 rounded-xs  {tags.selectedTag === tag ? ' border-primary-text' : 'border-transparent'}"
+            class="text-xs cursor-pointer  hover:px-1 border w-max  py-0.5 rounded-xs  {tags.selectedTag === tag ? ' border-primary-text px-1' : 'border-transparent'}"
             >
             {tag} {tags.selectedTag === tag ? 'x' : ''}
         </div>
