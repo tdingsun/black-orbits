a46287d33acc1eeb74eb1b398fc2f81724a54320
tdingsun <tiger.dingsun@gmail.com>
2025-05-08 07:09:35 -0400

    fix marginalia underline

--- src/lib/components/BlockContentMarginalia.svelte
+++ src/lib/components/BlockContentMarginalia.svelte
@@ -8,10 +8,20 @@
     let nextAnnotationLeft = siteState.nextAnnotationLeft;
     siteState.nextAnnotationLeft = !siteState.nextAnnotationLeft;
 
-    let showAnnotation = $state(false)
+    let showAnnotation = $state(false);
+    onMount(() => {
+        console.log(portableText.plainTextContent);
+        console.log(portableText.plainTextContent.slice(0, -1));
+    })
 </script>
-<span onclick={() => {showAnnotation = !showAnnotation}} class="group hover:font-bold hover:tracking-tight underline cursor-pointer">
-    {portableText.plainTextContent}<div class="{showAnnotation ? 'block' : 'hidden'}  sm:absolute {nextAnnotationLeft ? 'left-4' : 'right-4'} sm:w-[calc(50dvw-18rem)] sm:min-w-72 sm:max-w-100 bg-bg p-4 border hover:border-2 border-primary-text rounded-xs">
-        <img oncontextmenu={(e) => {e.preventDefault(); return false}} src={getImgUrl(portableText.value.image)} alt='' class="cursor-pointer "/>
-    </div>
+<span>
+
+</span>
+<span onclick={() => {showAnnotation = !showAnnotation}} class="group hover:font-bold hover:tracking-tight cursor-pointer">
+    <span class="underline">{portableText.plainTextContent}</span>
+        <div class="{showAnnotation ? 'block' : 'hidden'} my-4  sm:absolute {nextAnnotationLeft ? 'left-4 justify-end' : 'right-4 justify-start'} sm:w-[calc(50dvw-19rem)] sm:min-w-72 flex">
+            <div class="sm:max-w-100  p-4  hover:border  sm:border sm:hover:border-2 min-[1150px]:!border-0 min-[1150px]:hover:!border border-primary-text rounded-xs bg-bg">
+                <img oncontextmenu={(e) => {e.preventDefault(); return false}} src={getImgUrl(portableText.value.image)} alt='' class="cursor-pointer "/>
+            </div>
+        </div>
 </span>
