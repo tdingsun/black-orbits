180a819a585fb2fc9ed003b8289dc5af2abf336e
tdingsun <tiger.dingsun@gmail.com>
2025-07-31 12:37:32 -0400

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
